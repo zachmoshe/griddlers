0c86b81669c362680c8c80833da8b45de061e9a0
zachmoshe <zachmoshe@gmail.com>
2014-09-08 23:19:46 +0100

    adding core/bin directory

---
+++ core/bin/board_solver.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import sys
+import os
+sys.path.append("{}/..".format(os.path.dirname(__file__)))
+import griddlers
+import json 
+
+STRATEGIES = { 
+	'naive': griddlers.strategies.NaiveStrategy,
+	'naive-probs': griddlers.strategies.ProbsStrategy,
+}
+
+try:
+	strategy_params = json.loads(sys.stdin.readline())
+	request_params = json.loads(sys.stdin.readline())
+	board_obj = json.loads(sys.stdin.readline())
+	board = griddlers.Board.deserialize(board_obj)
+
+	# build strategy
+	if not strategy_params['name']:
+		raise Exception("Can't find 'name' in strategy")
+	if not strategy_params['name'] in STRATEGIES:
+		raise Exception("Unknown strategy {}".format(strategy_params['name']))
+	strategy = STRATEGIES[strategy_params['name']]
+	strategy_params.pop("name")
+
+	solver = griddlers.Solver(strategy, strategy_params, request_params)
+	res = solver.solve(board)
+
+	print(json.dumps(res))
+	exit(0)
+
+except Exception as ex:
+	err = { 'status': 'error', 'message': str(ex) }
+	print(json.dumps(err))
+	sys.stderr.write(ex)
+	exit(1)
+
+
+
