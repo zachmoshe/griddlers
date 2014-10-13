0cb9338681f09bb0e6c81ae3b893b574d2b485ea
zachmoshe <zachmoshe@gmail.com>
2014-10-13 16:42:50 +0300

    added support of ImpossibleBoardException to probs

--- core/griddlers/strategies/probs_strategy.py
+++ core/griddlers/strategies/probs_strategy.py
@@ -1,4 +1,4 @@
-from . import perms_utils
+from . import perms_utils, ImpossibleBoardException
 import numpy as np 
 
 
@@ -32,15 +32,20 @@ class ProbsStrategy(object):
 		if l-sum(seqs)-len(seqs)+1 < 0:
 			return cnts
 
+		had_legal_perm = False
 		for perm in possible_perms:
 			# check if perm is legal
 			if any( ( (perm==0) & (curr_probs==1) )  |  
 				      ( (perm==1) & (curr_probs==0) ) ):
 				continue
+			had_legal_perm = True
 			total_curr_probs = sum(curr_probs[ perm == 1 ])
 
 			cnts += perm * total_curr_probs
 
+		if not had_legal_perm:
+			raise ImpossibleBoardException()
+		
 		cnts[ curr_probs == 0 ] = 0
 		
 		# normalize - take out all 1s and split the rest according to the weights
