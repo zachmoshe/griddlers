d61e1774dc456aa1b879cc05298c6892c22dc33e
Zach Moshe <zachmoshe@gmail.com>
2016-02-17 22:15:14 +0200

    adding gchq christmas puzzle and showing also partial-success in board panel

--- frontend/app/scripts/services/board.js
+++ frontend/app/scripts/services/board.js
@@ -13,79 +13,79 @@ angular.module('griddlersApp')
 	.service('BoardService', function () {
 		var S3 = new AWS.S3();
 		Promise.promisifyAll(Object.getPrototypeOf(S3));
- 	
+
 		var RESPONSE_BETWEEN_POLLS_MS = 1000 * 60; // 1 minute
 
- 		function getRequestS3Location(workId) { 
+ 		function getRequestS3Location(workId) {
  			return { Bucket: window.griddlersConfig.s3WorkBucket, Key: workId + '/request' };
  		}
- 		function getResponseS3Location(workId) { 
+ 		function getResponseS3Location(workId) {
  			return { Bucket: window.griddlersConfig.s3WorkBucket, Key: workId + '/response' };
  		}
 
 
-		this.waitForWork = function(workId, statusUpdateCallback) { 
+		this.waitForWork = function(workId, statusUpdateCallback) {
 			return S3.headObjectAsync( getRequestS3Location(workId) )
-			.then(function() { 
+			.then(function() {
 
-				function getResponse(workId) { 
+				function getResponse(workId) {
 					return S3.getObjectAsync( getResponseS3Location(workId) )
-					.catch(function() { 
+					.catch(function() {
 						statusUpdateCallback("WAITING");
-						return Promise.delay(RESPONSE_BETWEEN_POLLS_MS).then(function() { 
+						return Promise.delay(RESPONSE_BETWEEN_POLLS_MS).then(function() {
 							return getResponse(workId);
 						});
 					});
 				}
 
-				// check if response is there and wait if it isn't 
+				// check if response is there and wait if it isn't
 				return getResponse(workId);
 			})
-			.then(function(resp) { 
+			.then(function(resp) {
 				var resp_body = JSON.parse(resp.Body);
-				if (resp_body.status === "success") { 
+				if (resp_body.status === "success" || resp_body.status === "partial-success") { 
 					return resp_body.iterations;
 				} else {
 					throw resp_body.status + ": " + resp_body.message;
 				}
 			})
-			.catch(function(ex) { 
-				if (ex.code === 'NotFound') { 
+			.catch(function(ex) {
+				if (ex.code === 'NotFound') {
 					throw "Illegal work ID";
 				} else {
 					throw ex;
 				}
 			});
-			
+
 		};
 
 		var _randomBoards = null;
-		this.getRandomBoards = function(numBoards) { 
-			if (_randomBoards) { 
+		this.getRandomBoards = function(numBoards) {
+			if (_randomBoards) {
 				return Promise.resolve(_randomBoards);
 			}
 
 			var that = this;
-			return S3.listObjectsAsync({ 
+			return S3.listObjectsAsync({
 				Bucket: window.griddlersConfig.s3WorkBucket
 			})
-			.then(function(resp) { 
+			.then(function(resp) {
 				var workIds = resp.Contents
-				.filter(function(file) { 
+				.filter(function(file) {
 					return file.Size > 200 && file.Key.endsWith('/response');
 				})
-				.map(function(file) { 
+				.map(function(file) {
 					return file.Key.split('/')[0];
 				})
 				.slice(0,numBoards);
 
-				return Promise.all(workIds.map(function(workId) { 
+				return Promise.all(workIds.map(function(workId) {
 					return that.waitForWork(workId, function(){})
-					.then(function(iterations) { 
+					.then(function(iterations) {
 						return { board: iterations.slice(-1)[0].board, workId: workId };
 					});
 				}))
-				.then(function(randomBoards) { 
+				.then(function(randomBoards) {
 					_randomBoards = randomBoards;
 					return randomBoards;
 				});
